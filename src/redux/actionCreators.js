bf20235e668a6f290383554e1589ce96e2e42c39
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-05 22:57:19 +0700

    see the code

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -5,3 +5,23 @@ export function toggleIsAdding() {
 export function addWord(en, vn) {
     return { type: 'ADD_WORD', en, vn };
 }
+
+export function toggleMemorized(id) {
+    return { type: 'TOGGLE_MEMORIZED', id };
+}
+
+export function toggleShow(id) {
+    return { type: 'TOGGLE_SHOW', id };
+}
+
+export function showNeedPractice() {
+    return { type: 'FILTER_NEED_PRACTICE' };
+}
+
+export function showAll() {
+    return { type: 'FILTER_SHOW_ALL' };
+}
+
+export function showMemorized() {
+    return { type: 'FILTER_MEMORIZED' };
+}
